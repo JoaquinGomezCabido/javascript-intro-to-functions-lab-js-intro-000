510ca60a4b996bd315540f78808df66f594c3d12
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-03 20:24:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+functions whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+functions logWhisper(string) {
+  console.console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase === string) {
+    return "I can't hear you!"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you too"
+  }
+}
